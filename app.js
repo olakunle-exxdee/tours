87fad683cafb81da9944d53efb8756d57cc0396f
olakunle_exdee <saheedogunsolu2016@gmail.com>
2020-11-11 12:05:16 +0100

    amination

--- app.js
+++ app.js
@@ -33,6 +33,36 @@ const person2 = document.querySelector(".person2");
 const texting = document.querySelector(".texting");
 let showMenu = false;
 
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
 hamburger.addEventListener("click", openMenu);
 
 function openMenu() {
